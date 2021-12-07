d5d45235a769c5079fee6822f2c53e323ace8513
jana-alam <janaalam2929@gmail.com>
2021-12-08 02:32:49 +0600

    Login & register with firebase implementation completed

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyArBZNY6b1b4L3_cgAB3NNoxoJNScy7rj0",
+  authDomain: "books-app-2e16a.firebaseapp.com",
+  projectId: "books-app-2e16a",
+  storageBucket: "books-app-2e16a.appspot.com",
+  messagingSenderId: "529763196996",
+  appId: "1:529763196996:web:e08d197bd509d0d1bf5c11",
+};
+
+export default firebaseConfig;
---
+++ src/hooks/useFireBase.js
@@ -0,0 +1,90 @@
+import { useEffect, useState } from "react";
+import {
+  getAuth,
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import firebaseInitialization from "firebase/firebase.init";
+import axios from "axios";
+import { useHistory } from "react-router";
+
+firebaseInitialization();
+
+const useFireBase = () => {
+  const [user, setUser] = useState({});
+  const history = useHistory();
+
+  const auth = getAuth();
+
+  useEffect(() => {
+    const unsubscribed = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser({});
+      }
+    });
+    return () => unsubscribed;
+  }, [auth]);
+
+  // register user
+  const registerUser = (name, email, password) => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        if (userCredential.user) {
+          const user = userCredential.user;
+          console.log(user);
+          axios({
+            method: "post",
+            url: "https://registertest.free.beeceptor.com/init",
+            data: {
+              userId: user.uid,
+              email: email,
+            },
+          })
+            .then((response) => {
+              //assume response is 200
+              // if response is 200 then
+              // history.push("/admin")
+              console.log(response);
+            })
+            .catch((error) => console.log(error))
+            .finally(() => history.push("/admin")); //assume response is 200 and redirected to admin page
+        }
+      })
+      .catch((error) => {});
+  };
+
+  //   login user
+  const loginUser = (email, password, history) => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        if (userCredential.user) {
+          const user = userCredential.user;
+          console.log(user);
+          axios({
+            method: "post",
+            url: "https://registertest.free.beeceptor.com/init",
+            data: {
+              userId: user.uid,
+              email: email,
+            },
+          })
+            .then((response) => {
+              //assume response is 200
+              // if response is 200 then
+              // history.push("/admin")
+              console.log(response);
+            })
+            .catch((error) => console.log(error))
+            .finally(() => history.push("/admin")); //assume response is 200 and redirected to admin page
+        }
+      })
+      .catch((error) => {});
+  };
+
+  return { user, registerUser, loginUser };
+};
+
+export default useFireBase;
